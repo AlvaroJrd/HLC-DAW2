753cc84fbadc35f5c26cd5acf72f673b592e7213
unknown <alvarojuradodam@gmail.com>
2023-01-10 10:01:18 +0100

    first commit

---
+++ Introduccion.py
@@ -0,0 +1,21 @@
+# Ejercicio 1
+"""  
+num1 = int(input("Introduzca el primer digito: "))
+num2 = int(input("Introduzca el segundo digito: "))
+
+print(f"La multiplicacion entre {num1} y {num2} es {num1*num2}")
+"""
+
+# Ejercicio 2
+"""
+text = "Tengo {totalMoney} euros para comprar {quantity} tarjetas gráficas por {price:.2f} dólares."
+
+print(text.format(totalMoney = 1000, quantity = 3, price = 450))
+"""
+
+# Ejercicio 3
+cadena1 = "Nombre" 
+cadena2 = "Es"
+cadena3 = "Ignatius"
+
+print(cadena1, cadena2,cadena3, sep="**")
---
+++ hola_mundo.py
@@ -0,0 +1,5 @@
+print("Hola Mundo!")
+
+nombre = input("Introduzca su nombre: ")
+
+print("Hola",nombre,", eres buena gente")
